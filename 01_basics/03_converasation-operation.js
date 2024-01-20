c4c206a82e14ff296d952cc70e198b79142a2aec
Divyprakash Pal <137271604+Divyprakashpal@users.noreply.github.com>
2024-01-20 08:26:39 +0000

    converation

---
+++ 01_basics/03_converasation-operation.js
@@ -0,0 +1,69 @@
+let score = "bravoo";
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "bravoo";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *********************** Operations ***********************
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);  (number  ke bad ** means power form hota hai )
+// console.log(2/3);
+// console.log(2%3);(% modular to find reminder)
+
+let str1 = "hello";
+let str2 = " Bravoo";
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2"); 
+
+// (phale value aur bad me string me value likhe per phale value plus hoti hai aur string value same print ho jati hai)
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter);
+
+//value ke phale sign assign karte hai to prefix bolte hai aur (++gamecounter)---- 
+// aur bad me karne per postprefix bolte hai(gamecounter)-----but answer same hi rahata hai..
