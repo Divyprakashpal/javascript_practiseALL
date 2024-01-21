e32c3fd948f83cc30f7eb6eb3bca67d972160110
Divyprakash Pal <137271604+Divyprakashpal@users.noreply.github.com>
2024-01-21 07:12:23 +0000

    change

---
+++ 01_basics/04_comperision.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);  -----------(value print in boolean form)
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1); (checked and compare)
+// console.log(2 != 1);=========(not equal to)
+
+// =====================  COMPARE KARTE TIME CHECKED KARNA HAO KI DATATYPE SAME HONA CHAIYE======================
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === (strickly checked DATATYPES)
+
+console.log("2" === 2);
